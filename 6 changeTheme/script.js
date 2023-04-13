7f8760f493fb4e56c931306bb394ed99f30c3296
Sameer Khadka <khadkasameer23@gmail.com>
2023-04-13 09:45:29 +0545

    dark & light theme update

---
+++ 6 changeTheme/script.js
@@ -0,0 +1,8 @@
+const bgColorDark =() => {
+    document.getElementById("body").style.backgroundColor = "black"
+    document.getElementById("body").style.color = "white"
+}
+const bgColorlight =() => {
+    document.getElementById("body").style.backgroundColor = "white"
+    document.getElementById("body").style.color = "black"
+}
